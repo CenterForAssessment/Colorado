ef0df7d3823f3da3dcffd91110145113749c4e2e
Adam VanIwaarden <avaniwaarden@gmail.com>
2016-02-29 21:33:53 -0700

    Add ELA 11th Grade and

--- SGP_CONFIG/2015/ELA.R
+++ SGP_CONFIG/2015/ELA.R
@@ -13,27 +13,27 @@ ELA_2015.config <- list(
 		sgp.content.areas=c("READING", "WRITING", "ELA"),
 		sgp.panel.years=c("2012", "2012", "2013", "2013", "2014", "2014", "2015"),
 		sgp.grade.sequences=list(c("3", "3", "4"), c("4", "4", "5"), c("5", "5", "6"),
-			c("6", "6", "7"), c("7", "7", "8"), c("8", "8", "9"), c("9", "9", "10")),
-		sgp.exact.grade.progression=list(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE),
-		sgp.projection.grade.sequences=list("NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS"),
+			c("6", "6", "7"), c("7", "7", "8"), c("8", "8", "9"), c("9", "9", "10"), c("10", "10", "11")),
+		sgp.exact.grade.progression=list(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE),
+		sgp.projection.grade.sequences=list("NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS"),
 		sgp.norm.group.preference=3),
 
 	ELA.2015 = list( # 2 Years of priors
 		sgp.content.areas=c("READING", "WRITING", "READING", "WRITING", "ELA"),
 		sgp.panel.years=c("2013", "2013", "2014", "2014", "2015"),
 		sgp.grade.sequences=list(c("3", "3", "4", "4", "5"), c("4", "4", "5", "5", "6"), c("5", "5", "6", "6", "7"), 
-			c("6", "6", "7", "7", "8"), c("7", "7", "8", "8", "9"), c("8", "8", "9", "9", "10")),
-		sgp.exact.grade.progression=list(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE),
-		sgp.projection.grade.sequences=list("NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS"),
+			c("6", "6", "7", "7", "8"), c("7", "7", "8", "8", "9"), c("8", "8", "9", "9", "10"), c("9", "9", "10", "10", "11")),
+		sgp.exact.grade.progression=list(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE),
+		sgp.projection.grade.sequences=list("NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS"),
 		sgp.norm.group.preference=2),
 
 	ELA.2015 = list( # 3 Years of priors
 		sgp.content.areas=c("READING", "WRITING", "READING", "WRITING", "READING", "WRITING", "ELA"),
 		sgp.panel.years=c("2012", "2012", "2013", "2013", "2014", "2014", "2015"),
-		sgp.grade.sequences=list(c("3", "3", "4", "4", "5", "5", "6"), c("4", "4", "5", "5", "6", "6", "7"), 
-			c("5", "5", "6", "6", "7", "7", "8"), c("6", "6", "7", "7", "8", "8", "9"), c("7", "7", "8", "8", "9", "9", "10")),
-		sgp.exact.grade.progression=list(TRUE, TRUE, TRUE, TRUE, TRUE),
-		sgp.projection.grade.sequences=list("NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS"),
+		sgp.grade.sequences=list(c("3", "3", "4", "4", "5", "5", "6"), c("4", "4", "5", "5", "6", "6", "7"), c("5", "5", "6", "6", "7", "7", "8"),
+			c("6", "6", "7", "7", "8", "8", "9"), c("7", "7", "8", "8", "9", "9", "10"), c("8", "8", "9", "9", "10", "10", "11")),
+		sgp.exact.grade.progression=list(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE),
+		sgp.projection.grade.sequences=list("NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS"),
 		sgp.norm.group.preference=1)
 
 )
