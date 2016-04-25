c54251e8572352fb645a7b12aad188f3b464f262
Adam VanIwaarden <avaniwaarden@gmail.com>
2016-04-25 05:50:15 -0600

    First commit of Colorado SGP documentation files.

---
+++ SGP_Reports/CO_Tech_Report_Makefile.R
@@ -0,0 +1,53 @@
+#######
+# 2015
+#######
+
+### Load required packages
+
+require(SGP)
+require(data.table)
+
+setwd("/Users/avi/Dropbox (SGP)/Github_Repos/Documentation/Colorado/SGP_Reports/2015")
+
+# load("/Users/avi/Dropbox/SGP/Colorado/Data/Colorado_SGP.Rdata")
+load("../../Colorado_SGP.Rdata")
+
+Colorado_SGP@Data$Most_Recent_Prior <- as.character(NA)
+Colorado_SGP@Data[, Most_Recent_Prior := sapply(strsplit(as.character(Colorado_SGP@Data$SGP_NORM_GROUP), "; "), function(x) rev(x)[2])]
+
+
+library(SGPreports)
+use.data.table()
+
+renderMultiDocument(rmd_input = "Colorado_SGP_Report_2015.Rmd",
+										output_format = c("HTML", "PDF"), #"EPUB", 
+										# cover_img="../img/cover.jpg",
+										# add_cover_title=TRUE, 
+										# cleanup_aux_files = FALSE,
+										pandoc_args = "--webtex")
+
+renderMultiDocument(rmd_input = "Appendix_A_2015.Rmd",
+										output_format = c("HTML", "PDF"))
+										# cover_img="../img/cover.jpg",
+										# add_cover_title=TRUE)#,
+										# cleanup_aux_files = FALSE)
+
+renderMultiDocument(rmd_input = "Appendix_C_2015.Rmd",
+										output_format = c("HTML", "PDF"),
+										# cleanup_aux_files = FALSE)
+										# cover_img="../img/cover.jpg",
+										add_cover_title=TRUE)#,
+
+renderMultiDocument(rmd_input = "Appendix_B.Rmd",
+										output_format = c("HTML", "PDF"),
+										# cover_img="../img/cover.jpg",
+										# add_cover_title=TRUE,
+										# self_contained = FALSE,
+										# cleanup_aux_files = FALSE,
+										pandoc_args = "--webtex")
+
+# renderMultiDocument(rmd_input = "Appendix_C_2015_SS.Rmd",
+# 										output_format = c("HTML", "PDF"), #, "EPUB", "DOCX"
+# 										# cleanup_aux_files = FALSE)
+# 										cover_img="../img/cover.jpg",
+# 										add_cover_title=TRUE)#,
