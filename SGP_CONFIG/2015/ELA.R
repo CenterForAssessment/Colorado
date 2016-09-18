f9424d902551eea6273810edee22d106a92b4f9b
dbetebenner <dbetebenner@gmail.com>
2016-09-18 03:49:27 -0400

    Tidying up ELA configs

--- SGP_CONFIG/2015/ELA.R
+++ SGP_CONFIG/2015/ELA.R
@@ -14,8 +14,8 @@ ELA_2015.config <- list(
 		sgp.panel.years=c("2014", "2014", "2015"),
 		sgp.grade.sequences=list(c("3", "3", "4"), c("4", "4", "5"), c("5", "5", "6"),
 			c("6", "6", "7"), c("7", "7", "8"), c("8", "8", "9")),
-		sgp.exact.grade.progression=list(list(TRUE), 8), # Repeat 'list(TRUE)' once for each grade/analysis
-		sgp.projection.grade.sequences=list(rep("NO_PROJECTIONS", 8)),
+		sgp.exact.grade.progression=list(list(TRUE), 6), # Repeat 'list(TRUE)' once for each grade/analysis
+		sgp.projection.grade.sequences=list(rep("NO_PROJECTIONS", 6)),
 		sgp.norm.group.preference=3),
 
 	ELA.2015 = list( # 2 Years of priors
@@ -23,8 +23,8 @@ ELA_2015.config <- list(
 		sgp.panel.years=c("2013", "2013", "2014", "2014", "2015"),
 		sgp.grade.sequences=list(c("3", "3", "4", "4", "5"), c("4", "4", "5", "5", "6"), c("5", "5", "6", "6", "7"), 
 			c("6", "6", "7", "7", "8"), c("7", "7", "8", "8", "9")),
-		sgp.exact.grade.progression=list(rep(list(TRUE), 7)),
-		sgp.projection.grade.sequences=list(rep("NO_PROJECTIONS", 7)),
+		sgp.exact.grade.progression=list(rep(list(TRUE), 5)),
+		sgp.projection.grade.sequences=list(rep("NO_PROJECTIONS", 5)),
 		sgp.norm.group.preference=2),
 
 	ELA.2015 = list( # 3 Years of priors
@@ -32,8 +32,8 @@ ELA_2015.config <- list(
 		sgp.panel.years=c("2012", "2012", "2013", "2013", "2014", "2014", "2015"),
 		sgp.grade.sequences=list(c("3", "3", "4", "4", "5", "5", "6"), c("4", "4", "5", "5", "6", "6", "7"), c("5", "5", "6", "6", "7", "7", "8"),
 			c("6", "6", "7", "7", "8", "8", "9")),
-		sgp.exact.grade.progression=list(rep(list(TRUE), 6)),
-		sgp.projection.grade.sequences=list(rep("NO_PROJECTIONS", 6)),
+		sgp.exact.grade.progression=list(rep(list(TRUE), 4)),
+		sgp.projection.grade.sequences=list(rep("NO_PROJECTIONS", 4)),
 		sgp.norm.group.preference=1)
 
 )
