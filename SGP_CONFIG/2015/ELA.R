483898fc0b5b13af0020aa1825d8268d93a5b999
dbetebenner <dbetebenner@gmail.com>
2016-09-18 03:42:09 -0400

    Tidying up 2015 ELA configuration

--- SGP_CONFIG/2015/ELA.R
+++ SGP_CONFIG/2015/ELA.R
@@ -13,8 +13,8 @@ ELA_2015.config <- list(
 		sgp.content.areas=c("READING", "WRITING", "ELA"),
 		sgp.panel.years=c("2014", "2014", "2015"),
 		sgp.grade.sequences=list(c("3", "3", "4"), c("4", "4", "5"), c("5", "5", "6"),
-			c("6", "6", "7"), c("7", "7", "8"), c("8", "8", "9"), c("9", "9", "10"), c("10", "10", "11")),
-		sgp.exact.grade.progression=list(rep(TRUE, 8)), # Repeat 'TRUE' once for each grade/analysis
+			c("6", "6", "7"), c("7", "7", "8"), c("8", "8", "9")),
+		sgp.exact.grade.progression=list(list(TRUE), 8), # Repeat 'list(TRUE)' once for each grade/analysis
 		sgp.projection.grade.sequences=list(rep("NO_PROJECTIONS", 8)),
 		sgp.norm.group.preference=3),
 
@@ -22,8 +22,8 @@ ELA_2015.config <- list(
 		sgp.content.areas=c("READING", "WRITING", "READING", "WRITING", "ELA"),
 		sgp.panel.years=c("2013", "2013", "2014", "2014", "2015"),
 		sgp.grade.sequences=list(c("3", "3", "4", "4", "5"), c("4", "4", "5", "5", "6"), c("5", "5", "6", "6", "7"), 
-			c("6", "6", "7", "7", "8"), c("7", "7", "8", "8", "9"), c("8", "8", "9", "9", "10"), c("9", "9", "10", "10", "11")),
-		sgp.exact.grade.progression=list(rep(TRUE, 7)),
+			c("6", "6", "7", "7", "8"), c("7", "7", "8", "8", "9")),
+		sgp.exact.grade.progression=list(rep(list(TRUE), 7)),
 		sgp.projection.grade.sequences=list(rep("NO_PROJECTIONS", 7)),
 		sgp.norm.group.preference=2),
 
@@ -31,8 +31,8 @@ ELA_2015.config <- list(
 		sgp.content.areas=c("READING", "WRITING", "READING", "WRITING", "READING", "WRITING", "ELA"),
 		sgp.panel.years=c("2012", "2012", "2013", "2013", "2014", "2014", "2015"),
 		sgp.grade.sequences=list(c("3", "3", "4", "4", "5", "5", "6"), c("4", "4", "5", "5", "6", "6", "7"), c("5", "5", "6", "6", "7", "7", "8"),
-			c("6", "6", "7", "7", "8", "8", "9"), c("7", "7", "8", "8", "9", "9", "10"), c("8", "8", "9", "9", "10", "10", "11")),
-		sgp.exact.grade.progression=list(rep(TRUE, 6)),
+			c("6", "6", "7", "7", "8", "8", "9")),
+		sgp.exact.grade.progression=list(rep(list(TRUE), 6)),
 		sgp.projection.grade.sequences=list(rep("NO_PROJECTIONS", 6)),
 		sgp.norm.group.preference=1)
 
