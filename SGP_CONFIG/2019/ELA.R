7fb99bc69121a14b2cd883fb321d6dd40138d16a
Adam VanIwaarden <avaniwaarden@gmail.com>
2019-07-15 12:19:49 -0600

    Add in 3rd prior for CMAS configs per Marie's request

--- SGP_CONFIG/2019/ELA.R
+++ SGP_CONFIG/2019/ELA.R
@@ -8,7 +8,7 @@ ELA.2019.config <- list(
 	ELA.2019 = list(
 		sgp.content.areas=rep("ELA", 3),
 		sgp.panel.years=c("2017", "2018", "2019"),
-		sgp.grade.sequences=list(c("3", "4"), c("3", "4", "5"), c("4", "5", "6"), c("5", "6", "7"), c("6", "7", "8")))
+		sgp.grade.sequences=list(c("3", "4"), c("3", "4", "5"), c("3", "4", "5", "6"), c("4", "5", "6", "7"), c("5", "6", "7", "8")))
 )
 
 ELA_PSAT_9.2019.config <- list(
--- SGP_CONFIG/2019/MATHEMATICS.R
+++ SGP_CONFIG/2019/MATHEMATICS.R
@@ -8,7 +8,7 @@ MATHEMATICS.2019.config <- list(
 	MATHEMATICS.2019 = list(
 		sgp.content.areas=rep("MATHEMATICS", 3),
 		sgp.panel.years=c("2017", "2018", "2019"),
-		sgp.grade.sequences=list(c("3", "4"), c("3", "4", "5"), c("4", "5", "6"), c("5", "6", "7"), c("6", "7", "8")),
+		sgp.grade.sequences=list(c("3", "4"), c("3", "4", "5"), c("3", "4", "5", "6"), c("4", "5", "6", "7"), c("5", "6", "7", "8")),
 		sgp.norm.group.preference=1L)
 )
 
